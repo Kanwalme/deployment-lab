8a5016415ccf92f1466443094f0b5369765a1973
Kanwal Mehmud <mehmudkanwal@gmail.com>
2022-07-27 15:56:43 -0500

    commit changes

--- server/index.js
+++ server/index.js
@@ -6,10 +6,9 @@ const app = express ()
 
 app.use(express.static(path.join(__dirname, "/../public")));
 
-app.get('/',function(req,res){
-    res.sendFile(path.resolve('public/index.html'))
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
-
 
 const port = process.env.PORT || 4005
 
